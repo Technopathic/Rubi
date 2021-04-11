1557c7b01995fa8872aec7799ae412eb10a73f08
Ren <nsoharab@gmail.com>
2021-04-11 21:53:37 +0300

    Updates

---
+++ precache-manifest.38af08c77bd4294c85948e52da7a6769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2128d0ba8ec9af4481ddbcd90eb3d8a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "580b16c46f473d72f51a",
+    "url": "./static/css/main.6a237f4e.chunk.css"
+  },
+  {
+    "revision": "b4f25ac529af9973ade2",
+    "url": "./static/js/2.fcaf2981.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.fcaf2981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580b16c46f473d72f51a",
+    "url": "./static/js/main.d48d7468.chunk.js"
+  },
+  {
+    "revision": "c556b62f4e73089c40b6",
+    "url": "./static/js/runtime-main.a3501975.js"
+  }
+]);
